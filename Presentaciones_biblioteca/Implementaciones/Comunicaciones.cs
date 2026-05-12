7dc2a5e34ad39522ff1ec6dae91c17bf14a7ac9e
ephem <ephem@DESKTOP-IK6HJ4R>
2026-05-11 20:51:23 -0500

    Ya la libreria de presentaciones estaba pero no tenia nada, le añadi Comunicaciones e IComunicaciones, en la clase de Comunicaciones te dejo todo explicado sobre como funciona ese lado del codigo, porque es medianamente enredador, mañana en clase hacemos el Razor

---
+++ Presentaciones_biblioteca/Implementaciones/Comunicaciones.cs
@@ -0,0 +1,140 @@
+﻿using Libreria_Presentacion.Interfaces;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Libreria_Presentacion.Implementaciones
+{
+    public class Comunicaciones: IComunicaciones
+    {
+
+        public async Task<Dictionary<string, object>> Ejecutar(Dictionary<string, object> datos)
+        {
+            var url = datos["Url"].ToString();
+            datos.Remove("Url");
+            var stringData = datos.ContainsKey("Entidad") ? //pregunta si la entidad existe, si existe la convierte en un json, sino, solo agrega corchetes
+                JsonConvert.SerializeObject(datos["Entidad"]) : "{}";
+            var body = new StringContent(stringData, Encoding.UTF8, "application/json"); // avisa que mandara la entidad convertida en json, en el tiopo de texto que la quiere y le avisa que esta mandando un json
+
+            var httpClient = new HttpClient(); //crea al cliente http, Esto es como abrir un navegador invisible desde código. Sirve para hacer peticiones a APIs o páginas web.
+            httpClient.Timeout = new TimeSpan(0, 4, 0); //si al intentar acceder se demora 4 minutos, cancela la petición (horas, minutos, segundos)
+
+            var message = await httpClient.GetAsync(url); // ingresa a la url, y pide informacion, como escribir una url en el navegador
+
+            if (!message.IsSuccessStatusCode)
+                throw new Exception("Error Comunicacion"); //si algo sucede con la peticion, ingresa un valor de error
+
+            var resp = await message.Content.ReadAsStringAsync(); //convierte la respuesta del servidor en texto
+            httpClient.Dispose(); httpClient = null; //destruye el httpClient
+
+            resp = Replace(resp); // llama al metodo de replace que esta abajo, que recibe la respuesta y modifica como se veran los datos 
+            return new Dictionary<string, object>() {
+                { "Valor", resp }// regresa el resultado de todo aquello
+            };
+        }
+
+
+        public async Task<Dictionary<string, object>> EjecutarPost(Dictionary<string, object> datos)
+        {
+            var url = datos["Url"].ToString();
+            datos.Remove("Url");
+            var stringData = datos.ContainsKey("Entidad") ? 
+                JsonConvert.SerializeObject(datos["Entidad"]) : "{}";
+            var body = new StringContent(stringData, Encoding.UTF8, "application/json"); 
+
+            var httpClient = new HttpClient();
+            httpClient.Timeout = new TimeSpan(0, 4, 0); 
+
+            var message = await httpClient.PostAsync(url, body); // como se envia el body al servidor, se necesita recibir en el metodo
+
+            if (!message.IsSuccessStatusCode)
+                throw new Exception("Error Comunicacion"); 
+
+            var resp = await message.Content.ReadAsStringAsync(); 
+            httpClient.Dispose(); httpClient = null; 
+
+            resp = Replace(resp);  
+            return new Dictionary<string, object>() {
+                { "Valor", resp }
+            };
+        }
+
+        public async Task<Dictionary<string, object>> EjecutarPut(Dictionary<string, object> datos)
+        {
+            var url = datos["Url"].ToString();
+            datos.Remove("Url");
+            var stringData = datos.ContainsKey("Entidad") ?
+                JsonConvert.SerializeObject(datos["Entidad"]) : "{}";
+            var body = new StringContent(stringData, Encoding.UTF8, "application/json");
+
+            var httpClient = new HttpClient();
+            httpClient.Timeout = new TimeSpan(0, 4, 0);
+
+            var message = await httpClient.PutAsync(url, body); // como se envia el body al servidor, se necesita recibir en el metodo
+
+            if (!message.IsSuccessStatusCode)
+                throw new Exception("Error Comunicacion");
+
+            var resp = await message.Content.ReadAsStringAsync();
+            httpClient.Dispose(); httpClient = null;
+
+            resp = Replace(resp);
+            return new Dictionary<string, object>() {
+                { "Valor", resp }
+            };
+        }
+
+        public async Task<Dictionary<string, object>> EjecutarDelete(Dictionary<string, object> datos)
+        {
+            var url = datos["Url"].ToString();
+            datos.Remove("Url");
+            var stringData = datos.ContainsKey("Entidad") ?
+                JsonConvert.SerializeObject(datos["Entidad"]) : "{}";
+            var body = new StringContent(stringData, Encoding.UTF8, "application/json");
+
+            var httpClient = new HttpClient();
+            httpClient.Timeout = new TimeSpan(0, 4, 0);
+
+            var request = new HttpRequestMessage //voy a crear una peticion http
+            {
+                Method = HttpMethod.Delete, // aqui decimos que la petición sera sobre borrar algo
+                RequestUri = new Uri(url), //aca decimos que la petición sera a esa url
+                Content = body // y aca metemos el contenido de la entidad dentrro
+            };
+
+            var message = await httpClient.SendAsync(request); // aca se envia la petición 
+
+            if (!message.IsSuccessStatusCode)
+                throw new Exception("Error Comunicacion");
+
+            var resp = await message.Content.ReadAsStringAsync();
+            httpClient.Dispose(); httpClient = null;
+
+            resp = Replace(resp);
+            return new Dictionary<string, object>() {
+                { "Valor", resp }
+            };
+        }
+
+        private string Replace(string resp)
+        {
+            return resp.Replace("\\\\r\\\\n", "")
+                .Replace("\\r\\n", "")
+                .Replace("\\", "")
+                .Replace("\\\"", "\"")
+                .Replace("\"", "'")
+                .Replace("'[", "[")
+                .Replace("]'", "]")
+                .Replace("'{'", "{'")
+                .Replace("\\\\", "\\")
+                .Replace("'}'", "'}")
+                .Replace("}'", "}")
+                .Replace("\\n", "")
+                .Replace("\\r", "")
+                .Replace("    ", "")
+                .Replace("'{", "{")
+                .Replace("\"", "")
+                .Replace("  ", "")
+                .Replace("null", "''");
+        }
+    }
+}
